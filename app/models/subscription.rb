384555fa4420e717418738f5c7b13006e3d0a164
Sebastian McKee <sebmcke@gmail.com>
2025-04-29 14:59:17 -0500

    Create customer_name method for subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,4 +2,8 @@ class Subscription < ApplicationRecord
     has_many :tea_subscriptions
     has_many :teas, through: :tea_subscriptions
     belongs_to :customer
+
+    def customer_name
+        "#{self.customer.first_name} #{self.customer.last_name}"
+    end
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe Subscription, type: :model do
         it { should have_many(:teas).through :tea_subscriptions }
         it { should belong_to :customer }
     end
+
+    describe "instance methods" do
+        before(:each) do
+            @customer = create(:customer, first_name: "John", last_name: "Doe")
+            @subscription = create(:subscription, customer: @customer)
+        end
+
+        describe "#customer_name" do
+            it "Should return a customers first and last name" do
+                expect(@subscription.customer_name).to eq("John Doe")
+            end
+        end
+    end
 end
