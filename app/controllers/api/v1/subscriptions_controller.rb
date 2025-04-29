de3f08f5ba74073b9c9ed98e375cfce67544959c
Sebastian McKee <sebmcke@gmail.com>
2025-04-29 16:01:09 -0500

    Create show method for subscriptions

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::SubscriptionsController < ApplicationController
     def index
         render json: SubscriptionsSerializer.serialize_subscriptions(Subscription.all)
     end
+
+    def show
+        render json: SubscriptionsSerializer.serialize_subscription(Subscription.find(params[:id]))
+    end
 end
