31c2cd223b0a6a026a1ce86c93cf7f8704387f18
Sebastian McKee <sebmcke@gmail.com>
2025-04-29 14:28:30 -0500

    fix typo in temperature column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_29_181241) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_29_192712) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,7 +45,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_29_181241) do
   create_table "teas", force: :cascade do |t|
     t.string "title"
     t.string "description"
-    t.integer "temerature"
+    t.integer "temperature"
     t.string "brew_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
