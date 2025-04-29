de5b77cce40c760825d5b6fcc5098dc6cdd35e5d
Sebastian McKee <sebmcke@gmail.com>
2025-04-29 13:16:54 -0500

    Fix models for new one to many relationship between customer and subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,5 @@
 class Subscription < ApplicationRecord
     has_many :tea_subscriptions
-    has_many :customer_subscriptions
     has_many :teas, through: :tea_subscriptions
-    has_many :customers, through: :customer_subscriptions
+    belongs_to :customer
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 RSpec.describe Customer, type: :model do
     describe "relstionships" do
-        it { should have_many :customer_subscriptions }
-        it { should have_many(:subscriptions).through :customer_subscriptions }
+        it { should have_many :subscriptions }
     end
 end
