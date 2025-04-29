d392754fe4d8d841009ece0e9ed31e7c038b104e
Sebastian McKee <sebmcke@gmail.com>
2025-04-29 12:18:29 -0500

    Create models for tea assosiations

---
+++ app/models/subscription.rb
@@ -0,0 +1,3 @@
+class Subscription < ApplicationRecor
+
+end
--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,3 +1,4 @@
 class Tea < ApplicationRecord
-    
+    has_many :tea_subscriptions
+    has_many :teas, through: :tea_subscriptions
 end
